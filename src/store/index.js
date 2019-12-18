2b579cdd660afd1a6c5fd57eca89fc882713aa2e
nasser <anasserabdrabo@gmail.com>
2019-12-19 00:26:05 +0200

    adding baseApiUrl to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    baseApiUrl: "https://localhost:5001/api/"
+  },
   mutations: {},
   actions: {},
   modules: {}
