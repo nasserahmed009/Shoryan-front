c3ff3554ea4d0d7ecf40e40852dae09939060cc0
nasser <anasserabdrabo@gmail.com>
2019-12-20 14:21:46 +0200

    drugs instead of listings

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
+    <Loading v-if="isLoading" />
     <Carousel />
-    <div class="container">
+
+    <div class="container" v-if="!isLoading">
       <h4 class="bold">Most ordered products</h4>
       <div class="row">
         <div
           class="col m3 s12"
-          v-for="item in items.slice(0, 8)"
-          :key="item.id"
+          v-for="drug in drugs.slice(0, 8)"
+          :key="drug.id"
         >
-          <ItemCard :item="item" />
+          <DrugCard :drug="drug" />
         </div>
       </div>
 
@@ -30,27 +32,31 @@
 <script>
 export default {
   mounted() {
-    this.getListings();
+    this.isLoading = true;
+    this.getDrugs();
     this.getCategories();
+    this.isLoading = false;
   },
   data() {
     return {
-      items: null, //items to be viewed in the index page
+      isLoading: false, // to show the preloader or not§1
+      drugs: null, //items to be viewed in the index page
       categories: null
     };
   },
   components: {
-    ItemCard: () => import("@/components/Home/ItemCard"),
+    DrugCard: () => import("@/components/Home/DrugCard"),
     Carousel: () => import("@/components/Home/Carousel"),
-    CategoryCard: () => import("@/components/Home/CategoryCard")
+    CategoryCard: () => import("@/components/Home/CategoryCard"),
+    Loading: () => import("@/components/Loading")
   },
   methods: {
-    async getListings() {
+    async getDrugs() {
       const response = await this.axios.get(
-        `${this.$store.state.baseApiUrl}Listings`
+        `${this.$store.state.baseApiUrl}drugs`
       );
 
-      this.items = response.data;
+      this.drugs = response.data;
     },
     async getCategories() {
       const response = await this.axios.get(
