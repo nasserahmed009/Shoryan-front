7e3decad6f2ca8f1c2acf52ca5a5cd72b98be433
nasser <anasserabdrabo@gmail.com>
2019-12-21 14:46:11 +0200

    routes validation and constraints

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import store from "@/store/index";
 
 Vue.use(VueRouter);
 
@@ -39,67 +40,48 @@ const HomeRoutes = [
         path: "/AddListing",
         component: () => import("@/views/Home/AddListing"),
         name: "AddListing",
-        meta: { title: "Shoryan - AddListing" }
+        meta: {
+          title: "Shoryan - AddListing",
+          requiresAuth: true,
+          roles: ["Normal", "Pharmacy"]
+        }
       },
       {
         path: "/Register",
         component: () => import("@/views/Home/AccountRegisteration"),
         name: "Register",
-        meta: { title: "Shoryan - AccountRegisteration" }
+        meta: { title: "Shoryan - AccountRegisteration", requiresNotAuth: true }
       },
       {
         path: "/Login",
         component: () => import("@/views/Home/UsersLogin.vue"),
         name: "Login",
-        meta: { title: "Shoryan - UsersLogin" }
+        meta: { title: "Shoryan - UsersLogin", requiresNotAuth: true }
       },
       {
         path: "/Logout",
         component: () => import("@/views/Home/Logout.vue"),
         name: "Logout",
-        meta: { title: "Shoryan - Logout" }
+        meta: { title: "Shoryan - Logout", requiresAut: true }
       },
-      {
-        path: "/ActiveListings",
-        component: () => import("@/views/Home/ActiveListings.vue"),
-        name: "ActiveListings",
-        meta: { title: "Shoryan - ActiveListings" }
-      },
+
       {
         path: "/MyCart",
         component: () => import("@/views/Home/MyCart"),
         name: "MyCart",
-        meta: { title: "Shoryan - MyCart" }
+        meta: { title: "Shoryan - MyCart", requiresAuth: true }
       },
       {
         path: "/Profile",
         component: () => import("@/views/Home/Profile"),
         name: "Profile",
-        meta: { title: "Shoryan - Test" }
+        meta: { title: "Shoryan - Test", requiresAuth: true }
       },
       {
         path: "/Profile/Edit",
         component: () => import("@/views/Home/EditProfileSettings"),
         name: "EditProfile",
-        meta: { title: "Shoryan - Edit profile" }
-      },
-      {
-        path: "/PastOrders",
-        component: () => import("@/views/Home/PastOrders"),
-        name: "PastOrders",
-        meta: { title: "Shoryan - PastOrders" }
-      },
-      {
-        path: "/ProductView",
-        component: () => import("@/views/Home/ProductView"),
-        name: "ProductView",
-        meta: { title: "Shoryan - ProductView" }
-      },
-      {
-        path: "/UpcomingOrders",
-        component: () => import("@/views/Home/UpcomingOrders"),
-        name: "UpcomingOrders",
-        meta: { title: "Shoryan - UpcomingOrders" }
+        meta: { title: "Shoryan - Edit profile", requiresAuth: true }
       }
     ]
   },
@@ -111,49 +93,69 @@ const HomeRoutes = [
         path: "",
         component: () => import("@/views/Panel/Dashboard"),
         name: "Dashboard",
-        meta: { title: "Panel - Dashboard" }
+        meta: {
+          title: "Panel - Dashboard",
+          requiresAuth: true,
+          roles: ["Admin"]
+        }
       },
       {
         path: "users",
         component: () => import("@/views/Panel/ListUsers"),
         name: "listUsers",
-        meta: { title: "Panel - Users " }
+        meta: { title: "Panel - Users ", requiresAuth: true, roles: ["Admin"] }
       },
       {
         path: "drugs",
         component: () => import("@/views/Panel/ListDrugs"),
         name: "listDrugs",
-        meta: { title: "Panel - Drugs " }
+        meta: { title: "Panel - Drugs ", requiresAuth: true, roles: ["Admin"] }
       },
       {
         path: "addDrug",
         component: () => import("@/views/Panel/AddDrug"),
         name: "addDrug",
-        meta: { title: "Panel - add drug " }
+        meta: {
+          title: "Panel - add drug ",
+          requiresAuth: true,
+          roles: ["Admin"]
+        }
       },
       {
         path: "items",
         component: () => import("@/views/Panel/ListItems"),
         name: "ListItems",
-        meta: { title: "Panel - Items " }
+        meta: { title: "Panel - Items ", requiresAuth: true, roles: ["Admin"] }
       },
       {
         path: "giftCards",
         component: () => import("@/views/Panel/ListGiftCards"),
         name: "listGiftCards",
-        meta: { title: "Panel - list gift cards " }
+        meta: {
+          title: "Panel - list gift cards ",
+          requiresAuth: true,
+          roles: ["Admin"]
+        }
       },
       {
         path: "addGiftCard",
         component: () => import("@/views/Panel/AddGiftCard"),
         name: "addGiftCard",
-        meta: { title: "Panel - add gift card" }
+        meta: {
+          title: "Panel - add gift card",
+          requiresAuth: true,
+          roles: ["Admin"]
+        }
       },
       {
         path: "complaints",
         component: () => import("@/views/Panel/Complaints"),
         name: "Complaints",
-        meta: { title: "Panel - Complaints " }
+        meta: {
+          title: "Panel - Complaints ",
+          requiresAuth: true,
+          roles: ["Admin"]
+        }
       }
     ]
   }
@@ -168,3 +170,30 @@ const router = new VueRouter({
 });
 
 export default router;
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title; //the title of the page that appears at the top
+
+  const loggedIn = store.getters.loggedIn; //get if a user is logged in or not
+  const loggedInUser = store.state.user; //get the logged in user info
+  const userRole = loggedIn ? loggedInUser.type : null; //get the logged in user roles
+
+  //check if the route to be accessed requires no auth
+  const requiresNotAuth = to.matched.some(
+    record => record.meta.requiresNotAuth
+  );
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth); //check if the routed requires to be authenticated
+  const roles = to.meta.roles; //accessed routes roles
+
+  if (requiresNotAuth && loggedIn) next({ name: "Index" });
+  else if (requiresAuth && !loggedIn) next({ name: "Login" });
+  else if (requiresAuth && loggedIn && roles) {
+    for (let routeRole of roles) {
+      if (routeRole == userRole) {
+        next();
+        return;
+      }
+    }
+    next({ name: "Index" });
+  } else next();
+});
--- src/views/Home/ProductView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <div class="container" style="margin-top:2%"></div>
-  </div>
-</template>
-
-<style></style>
