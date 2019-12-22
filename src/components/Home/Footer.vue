c2d4b293f1dfcf8d56ce8f8bc7802269c94a0f6f
Youssef Walid <yousefwalid98@hotmail.com>
2019-12-22 05:50:09 +0200

    Edited footer

--- src/components/Home/Footer.vue
+++ src/components/Home/Footer.vue
@@ -4,18 +4,31 @@
       <div class="container">
         <div class="row">
           <div class="col l6 s12">
-            <h5 class="white-text">Footer Content</h5>
+            <h5 class="white-text">Shoryan</h5>
             <p class="grey-text text-lighten-4">
-              You can use rows and columns here to organize your footer content.
+              We hope our service is healing you
             </p>
           </div>
           <div class="col l4 offset-l2 s12">
             <h5 class="white-text">Links</h5>
             <ul>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-              <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              <router-link :to="{ name: 'AllCategories' }"
+                ><li>
+                  <a class="grey-text text-lighten-3" href="#!"
+                    >All Categories</a
+                  >
+                </li></router-link
+              >
+              <router-link :to="{ name: 'AllDrugs' }"
+                ><li>
+                  <a class="grey-text text-lighten-3" href="#!">All Drugs</a>
+                </li></router-link
+              >
+              <router-link :to="{ name: 'Profile' }"
+                ><li>
+                  <a class="grey-text text-lighten-3" href="#!">Profile</a>
+                </li></router-link
+              >
             </ul>
           </div>
         </div>
@@ -23,7 +36,6 @@
       <div class="footer-copyright">
         <div class="container">
           © 2019 Shoryan
-          <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
         </div>
       </div>
     </footer>
